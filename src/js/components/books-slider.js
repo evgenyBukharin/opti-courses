0480872e00183a4b989191005016161f2a135104
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-06-05 12:19:47 +0500

    books slider done

--- src/js/components/books-slider.js
+++ src/js/components/books-slider.js
@@ -1,7 +1,22 @@
 import Swiper, { Autoplay } from "swiper";
 Swiper.use([Autoplay]);
+const swiperContainer = document.querySelector(".module-books-slider__slider");
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(swiperContainer, {
 	slidesPerView: 4,
-	// spaceBetween: 10,
+	spaceBetween: 50,
+	watchSlidesProgress: true,
+	slideVisibleClass: "module-books-slider__slide-visible",
+	autoplay: {
+		delay: 5000,
+		pauseOnMouseEnter: true,
+		disableOnInteraction: false,
+	},
+	on: {
+		afterInit: () => {
+			const slides = swiperContainer.querySelectorAll(".swiper-slide");
+			const maxIndexSpan = document.querySelector(".module-books-slider__maxindex");
+			maxIndexSpan.innerHTML = slides.length;
+		},
+	},
 });
